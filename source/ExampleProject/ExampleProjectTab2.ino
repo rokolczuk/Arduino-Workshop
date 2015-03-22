a5fec0986d6e3e5ef6b29ae7e7022fa51e9abebb
rokolczuk <rokolczuk@Andrzejs-Mini.home>
2015-03-22 17:11:50 +0000

    implemented anotherCoolMethod

--- source/ExampleProject/ExampleProjectTab2.ino
+++ source/ExampleProject/ExampleProjectTab2.ino
@@ -5,3 +5,11 @@ void someCoolMethod()
   digitalWrite(13, LOW);
   delay(500);
 }
+
+void anotherCoolMethod()
+{
+  digitalWrite(13, HIGH);
+  delay(700);
+  digitalWrite(13, LOW);
+  delay(700);
+}
